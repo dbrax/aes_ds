6b660c5051735b08a60ae59fac79da896534f6d4
Emmanuel <epmnzava@gmail.com>
2024-11-02 17:25:20 +0300

    updated code

---
+++ src/AeDs.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Epmnzava\AeDs;
+
+use Exception;
+
+class AeDs
+{
+    protected $api_key;
+    protected $api_secret;
+    public function __construct() {}
+
+    public function getAccessToken($app_key, $app_secret)
+    {
+        $action = "/auth/token/create";
+
+        // $client = new IopClientImpl($url, $appkey, $appSecret);
+        $client = new IopClient(UrlConstants::$api_authorization_url, $app_key, $app_secret);
+        $request = new IopRequest($action);
+        //$request->setApiName($action);
+        // $request->addApiParameter("code", "your_code");
+
+        try {
+            $response = $client->execute($request, null);
+            echo json_encode($response);
+            //   echo json_encode($response->getGopResponseBody());
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
---
+++ src/UrlConstants.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Epmnzava\AeDs;
+
+class UrlConstants
+{
+    static $api_gateway_url_tw = "https://api-sg.aliexpress.com/sync";
+    static $api_authorization_url = "https://api-sg.aliexpress.com/rest";
+}

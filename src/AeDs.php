074747a5050e569292fb12eb81d5e13fadd5fc8e
Emmanuel <epmnzava@gmail.com>
2024-11-18 15:50:24 +0300

    updated code

--- src/AeDs.php
+++ src/AeDs.php
@@ -20,6 +20,8 @@ class AeDs
         $this->code = $code;
     }
 
+
+
     public function getAccessToken()
     {
         $action = "/auth/token/create";
@@ -41,6 +43,54 @@ class AeDs
         }
     }
 
+
+    public function getProduct($product_id, $currency = "TZS", $language = "en", $shipCountryCode = "TZ",)
+    {
+        $c = new IopClient(UrlConstants::$api_gateway_url_tw, $this->app_key, $this->app_secret);
+        $request = new IopRequest('aliexpress.ds.product.get');
+
+        $request->addApiParam('ship_to_country', $shipCountryCode);
+        $request->addApiParam('product_id', $product_id);
+        $request->addApiParam('target_currency', $currency);
+        $request->addApiParam('target_language', $language);
+        $res = $c->execute($request);
+
+        return $res;
+    }
+
+    public  function getProducts($category_id, $search_term = null, $result_size = 200, $feed_name = "DS bestseller")
+    {
+        $this->getAccessToken();
+        $c = new IopClient(UrlConstants::$api_gateway_url_tw, $this->app_key, $this->app_secret);
+        $request = new IopRequest('aliexpress.ds.feed.itemids.get');
+        $request->addApiParam('page_size', $result_size);
+        $request->addApiParam('category_id', $category_id);
+        $request->addApiParam('feed_name', $feed_name);
+        $request->addApiParam('search_id', $search_term);
+        $res = $c->execute($request,  $this->refresh_token);
+        return $res;
+    }
+
+    public function search_product($keyword, $currency = "TZS", $sort_by = "min_price,asc", $categoryid = null, $local = "zh_CN", $countryCode = "TZ", $page_size = "20", $pageIndex = 1)
+    {
+
+        $c = new IopClient(UrlConstants::$api_gateway_url_tw, $this->app_key, $this->app_secret);
+        $request = new IopRequest('aliexpress.ds.text.search');
+        $request->addApiParam('keyWord', $keyword);
+        $request->addApiParam('local', $local);
+        $request->addApiParam('countryCode', $countryCode);
+        $request->addApiParam('categoryId', $categoryid);
+        $request->addApiParam('sortBy', $sort_by);
+        $request->addApiParam('pageSize', $page_size);
+        $request->addApiParam('pageIndex', $pageIndex);
+        $request->addApiParam('currency', $currency);
+
+        $this->getAccessToken();
+
+        $res = $c->execute($request, $this->refresh_token);
+
+        return $res;
+    }
     public function getCategories($lang)
     {
         //$token = $this->getAccessToken();
